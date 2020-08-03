2f03914eb8c7b6d7a248ac3fc9a150adc5182fd4
Bashiru Suleman <exbashorun1@yahoo.com>
2020-08-03 10:20:06 -0600

    add multi-units to application

--- weight converter/main.js
+++ weight converter/main.js
@@ -5,9 +5,41 @@ document.getElementById('output').style.visibility = 'hidden';
 
 //converter function
 lbs.addEventListener('input', function(e){
- document.getElementById('output').style.visibility = 'visible';
  let pounds =  e.target.value;
- document.getElementById('gramsOutputs').innerHTML = (453.592 * pounds);
- document.getElementById('kgOutputs').innerHTML = (0.453592 * pounds);
- document.getElementById('ozOutputs').innerHTML = (16 * pounds);
- })
+ let units = document.getElementById('units').value;
+  // Function to loop through IDs to change visibility
+  function changeVisibility(ids){
+      for (let i = 0; i<ids.length; i++){
+          document.getElementById(ids[i]).style.visibility ='visible';
+      }
+  }
+ if (units=== 'lbs'){
+    changeVisibility(["grams", "kilo", "oz"]);
+    document.getElementById('gramsOutputs').innerHTML = (453.592 * pounds);
+    document.getElementById('kgOutputs').innerHTML = (0.453592 * pounds);
+    document.getElementById('ozOutputs').innerHTML = (16 * pounds);
+ }  else if (units=== 'kg') {
+    changeVisibility(["grams", "pounds", "oz"]);
+    //Remove the display of Kilograms 
+    document.getElementById('kilo').style.display = 'none';
+    document.getElementById('gramsOutputs').innerHTML = (453.592 * pounds);
+    document.getElementById('kgOutputs').innerHTML = (0.453592 * pounds);
+    document.getElementById('ozOutputs').innerHTML = (16 * pounds);
+ }
+ else if (units=== 'grams') {
+    changeVisibility(["kilo", "pounds", "oz"]);
+    document.getElementById('grams').style.display = 'none';
+    document.getElementById('gramsOutputs').innerHTML = (453.592 * pounds);
+    document.getElementById('kgOutputs').innerHTML = (0.453592 * pounds);
+    document.getElementById('ozOutputs').innerHTML = (16 * pounds);
+ }
+ else if (units=== 'ounces') {
+    document.getElementById('output').style.visibility = 'visible';
+    document.getElementById('gramsOutputs').innerHTML = (453.592 * pounds);
+    document.getElementById('kgOutputs').innerHTML = (0.453592 * pounds);
+    document.getElementById('ozOutputs').innerHTML = (16 * pounds);
+ }
+})
+
+
+    
